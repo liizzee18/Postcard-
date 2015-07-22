1bd8728198309372ae33f9f26cc2e3463e62acdc
Jesus Barragan <mom@Jesuss-Air.attlocal.net>
2015-07-21 18:25:53 -0600

    Initial Commit

---
+++ PostCard2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PostCard2
+//
+//  Created by Jesus Barragan on 7/21/15.
+//  Copyright (c) 2015 Elizabeth Barragan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
